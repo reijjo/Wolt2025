9f6b27d5c525841833024e04ab25e63d504f1ab9
reijjo <teemu.aitomeri@gmail.com>
2025-01-14 15:59:21 +0200

    data parsers done

--- src/components/homepage/Home.tsx
+++ src/components/homepage/Home.tsx
@@ -1,12 +1,31 @@
+import { SyntheticEvent } from 'react'
 import { Button } from '../common/Button'
 import { TextInput } from '../common/TextInput'
 import './Home.css'
+import { formatValue, parseName } from '../../utils/helperFunctions'
 
 export const Home = () => {
+	const sampleData = {
+		"cartValue": 1000,
+		"smallOrderSurcharge": 0,
+		"deliveryFee": 190,
+		"deliveryDistance": 1050,
+		"totalPrice": 1190
+	}
+
+
+
+	const calculatePrice = (e: SyntheticEvent) => {
+		e.preventDefault()
+		console.log('Calculating price...', sampleData)
+	}
+
+	console.log('SAMPLE DATA', Object.values(sampleData).map(item => item))
+
 	return (
 		<main className="wrapper">
 			<h1 className='main-header'>Delivery Order Price Calculator</h1>
-				<form className='form-details'>
+				<form className='form-details' onSubmit={calculatePrice}>
 					<TextInput label='Venue slug' name='venue' id='venue' dataTestId='venuSlug' placeholder='Venue...'/>
 					<TextInput label='Cart value (EUR)' name='cart' id='cart' dataTestId='cartValue' placeholder='Value...'/>
 					<TextInput label='User latitude' name='latitude' id='latitude' dataTestId='userLatitude' placeholder='Latitude...'/>
@@ -16,25 +35,13 @@ export const Home = () => {
 				<div className='price-breakdown'>
 					<h5>Price breakdown</h5>
 					<div className='breakdown-details'>
-						<div className='breakdown-item'>
-							<p>Cart Value</p> <span data-raw-value='1000'>10.00 &euro;</span>
+						{Object.entries(sampleData).sort().map(([key, value]) => (
+							<div className={`breakdown-item`} key={key}>
+								<p>{parseName(key)}</p> <span data-raw-value={value}>{formatValue(key, value)}</span>
 							</div>
-							<div className='breakdown-item'>
-							<p>Delivery fee</p> <span data-raw-value='190'>1.90 &euro;</span>
-							</div>
-							<div className='breakdown-item'>
-
-							<p>Delivery distance</p> <span data-raw-value='177'>177 m</span>
-							</div>
-							<div className='breakdown-item'>
-
-							<p>Small order surcharge</p> <span data-raw-value='000'>0.00 &euro;</span>
-							</div>
-							<div className='breakdown-item'>
-
-							<p>Total price</p> <span data-raw-value='1190'>11.90 &euro;</span>
-						</div>
+						))}
 					</div>
+
 				</div>
 		</main>
 	)
---
+++ src/utils/helperFunctions.ts
@@ -0,0 +1,20 @@
+
+	export const parseMoney = (value: number): string => {
+		return `${(value / 100).toFixed(2)} €`
+	}
+
+	export const parseDistance = (distance: number): string => {
+		return distance >= 1000 ? `${(distance / 1000).toFixed(2)} km` : `${distance} m`
+	}
+
+	export const formatValue = (key: string, value: number) => {
+		if (key === 'deliveryDistance') {
+			return parseDistance(value)
+		}
+		return parseMoney(value)
+	}
+
+	export	const parseName = (name: string) => {
+			const addSpace = name.replace(/([A-Z])/g, ' $1').trim();
+			return addSpace.charAt(0).toUpperCase() + addSpace.slice(1).toLowerCase();
+		};
