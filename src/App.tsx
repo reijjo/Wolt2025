36fed0997ad35e6ead9a58f1d14f367a54a44ec9
reijjo <teemu.aitomeri@gmail.com>
2025-01-14 09:00:45 +0200

    ok started

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Home } from "./components/homepage/Home"
+
+function App() {
+
+  return (
+    <Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<Home />} />
+			</Routes>
+		</Router>
+  )
+}
+
+export default App
